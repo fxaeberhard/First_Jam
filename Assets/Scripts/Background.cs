73f250c1a708bdaf30335333dc6c8d0151b650f3
guillaume volgyesi <guillaume volgyesi>
2017-01-22 02:32:48 +0100

    i need to sleep

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class Background : MonoBehaviour {
-
-	// Use this for initialization
-	void Awake ()
+public class Background : MonoBehaviour
+{
+   
+    // Use this for initialization
+    void Awake ()
     {
         DontDestroyOnLoad(transform.gameObject);
+       
+        
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+      
+    }
 }
---
+++ Assets/Scripts/Sattelite.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sattelite : MonoBehaviour
+{
+ 
+    
+    [SerializeField]GameObject Spawn;
+    [SerializeField]GameObject OutOfNowhere;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(Spawn);
+        DontDestroyOnLoad(OutOfNowhere);
+    }
+
+    void Start ()
+    {
+        
+    }
+	
+	
+	void Update ()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime );
+        transform.Translate(Vector3.down * Time.deltaTime / 8);
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "OutOfNowhere")
+        {
+            transform.position = Spawn.transform.position;
+        }
+    }
+}
